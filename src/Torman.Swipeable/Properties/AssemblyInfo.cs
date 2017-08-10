1e8c3a3c2a32d2d4a170b555b40799b1418f4f48
Dominik Przywara <dominik.przywara@gmail.com>
2017-08-10 14:08:59 +0200

    Added new version of DLL and changed Assembly version number

--- src/Torman.Swipeable/Properties/AssemblyInfo.cs
+++ src/Torman.Swipeable/Properties/AssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Torman.Swipeable")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("2.0.1")]
-[assembly: AssemblyFileVersion("2.0.1")]
+[assembly: AssemblyVersion("2.0.2")]
+[assembly: AssemblyFileVersion("2.0.2")]
